f26c40b6e5a4ec9aecdedfb447d434444fdbdd7a
trapholov <trapholov@gmail.com>
2018-11-25 08:48:15 +0300

    init

--- server.js
+++ server.js
@@ -116,7 +116,6 @@ app.get('*', isProd ? render : (req, res) => {
   readyPromise.then(() => render(req, res));
 });
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`server started at http://localhost:${port}`);
+app.listen('9292', () => {
+  console.log(`server started at http://localhost:9292`);
 });
